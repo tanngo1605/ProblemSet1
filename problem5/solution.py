90821d71acd178d3d08f54c416faeb79d5563902
tanngo1605 <50272836+tanngo1605@users.noreply.github.com>
2020-03-29 01:41:17 -0400

    Add files via upload

---
+++ problem5/solution.py
@@ -0,0 +1,9 @@
+#This is a really cool example of using closures to store data. 
+# We must look at the signature type of cons to retrieve its first and last elements. cons takes in a and b, and returns a new anonymous function, which itself takes in f, and calls f with a and b. So the input to car and cdr is that anonymous function, which is pair. To get a and b back, we must feed it yet another function, one that takes
+#  in two parameters and returns the first (if car) or last (if cdr) one.
+
+def car (pair)
+    return pair(lambda a, b:a)
+
+def  cdr(pair)
+    return pair(lambda a,b: b)
